033afcaa9719f87a1b2ac525a0803ab7020fb6df
Sharif Paget <sharif.paget@journalism.cuny.edu>
2018-03-05 14:57:50 -0500

    mobile edits

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
-$(document).ready(function(e){
+$(document).ready(function(){
+
+  $(".spot").on("click", function(e){
+    e.preventDefault();
 
 
+  var clicked_spot = $(this);
+  var des = clicked_spot.data("des");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  $(".modal").find(".inner").find("p").text(des);
+  $(".modal").addClass("active");
 });
+
+  $(".close").on("click", function (e){
+    e.preventDefault();
+
+  $(".modal").removeClass("active");
+});
+
+  });
